b727dcc6d84eb3093bbea2d4b54718db6cbd6818
Alexey Osipov <osipov27032002@mail.ru>
2023-04-27 22:33:16 +0400

    basic message handler

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from aiogram import Bot, types
-from aiogram.dispatcher import dispatcher
+from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-
 import os
 
+token_path = open('token.txt')
+token = token_path.read()
+bot = Bot(token=token)
+dp = Dispatcher(bot=bot)
+
+
+async def on_startup(_):
+    print('Бот вышел в онлайн')
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nНапиши мне что-нибудь!")
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Напиши мне что-нибудь, и я отправлю этот текст тебе в ответ!")
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
 
 
 if __name__ == '__main__':
-    pass
+    executor.start_polling(dp, skip_updates=True, on_startup = on_startup)
